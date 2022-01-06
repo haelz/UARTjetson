6ee818caa891aadbd6907ca9dac49f5acd564e58
haelz <37329062+haelz@users.noreply.github.com>
2022-01-06 20:51:46 +0100

    Update candelet.py

--- candelet.py
+++ candelet.py
@@ -6,8 +6,10 @@ import serial
 
 filepath= 'dhtdata.csv'
 exist=os.path.isfile(filepath)
-header = ['Timestamp', 'Temperature', 'Humidity']
-csvfile = open(filepath, 'a',newline='')
+header = ["Timestamp","Temperature","Humidity"]
+csvfile = open(filepath, 'a')
+writer = csv.DictWriter(csvfile,delimiter=",",lineterminator='\n', fieldnames = header)
+
 serial_port = serial.Serial(
     port="/dev/ttyTHS1",
     baudrate=115200,
@@ -18,28 +20,27 @@ serial_port = serial.Serial(
 # Wait a second to let the port initialize
 time.sleep(1)
 
+#if file not exist write header
+if not exist:
+    writer.writeheader()
+    
 try:
-    serial_port.write("UART test\r\n".encode())
+    #serial_port.write("UART test\r\n".encode())
     while True:
         if serial_port.inWaiting() > 0:
             data = serial_port.readline()
             print(data)
-            
-            writer = csv.DictWriter(csvfile,delimiter=",",lineterminator='\n', fieldnames = header)
-            #if file not exist write header
-            if not exist:
-                writer.writeheader()
-                
+             
             # split the data
-            testsplit=test.split(";")
-            temp = testsplit[0]
-            hum = testsplit[1]
+            split=data.decode("utf-8").split(";")
+            temp = split[0]
+            hum = split[1]
             now = datetime.now()
             #change timeformat
             t = now.strftime('%Y-%m-%d %H:%M:%S')
             
             #write the data
-            writer.writerows([{'Timestamp':t, 'Temperature':temp, 'Humidity':hum}])
+            writer.writerow({"Timestamp":t, "Temperature":temp, "Humidity":hum})
 except KeyboardInterrupt:
     print("Exiting Program")
     
@@ -51,3 +52,4 @@ finally:
     serial_port.close()
     csvfile.close()
     pass
+
